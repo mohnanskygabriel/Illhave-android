598b55afc402dda8c7eb83c10df9213255845571
Gabriel Mohňanský <mohnanskygabriel@gmail.com>
2018-06-17 10:30:56 +0200

    Z mojich objednavok sa dostanem k detailom objednavky

--- app/src/main/java/sk/upjs/sk/illhave/activity/MojeObjednavkyActivity.java
+++ app/src/main/java/sk/upjs/sk/illhave/activity/MojeObjednavkyActivity.java
@@ -2,6 +2,7 @@ package sk.upjs.sk.illhave.activity;
 
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -10,7 +11,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
+import lombok.val;
 import sk.upjs.sk.illhave.R;
 import sk.upjs.sk.illhave.localdb.IllHaveContract;
 
@@ -18,19 +21,28 @@ public class MojeObjednavkyActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<Cursor>, AdapterView.OnItemLongClickListener {
 
     private static final int LOADER_ID = 3;
-    SimpleCursorAdapter adapter;
+    private SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moje_objednavky);
-        ListView objednavkyView = findViewById(R.id.mojeObjednakyListView);
-        String[] from = {IllHaveContract.Order.TOTAL, IllHaveContract.Order.CREATED_DATE};
-        int[] to = {R.id.objednavka_lokalna_total_textView, R.id.objednavka_lokalna_cas_textView};
+        final ListView objednavkyView = findViewById(R.id.mojeObjednakyListView);
+        String[] from = {IllHaveContract.Order.TOTAL, IllHaveContract.Order.CREATED_DATE, IllHaveContract.Order.API_ID};
+        int[] to = {R.id.objednavka_lokalna_total_textView, R.id.objednavka_lokalna_cas_textView, R.id.objednavka_lokalna_apiId_textView};
         adapter = new SimpleCursorAdapter(
                 this, R.layout.objednavka_lokalna, null, from, to, 0);
         objednavkyView.setAdapter(adapter);
         getLoaderManager().initLoader(LOADER_ID, Bundle.EMPTY, this);
+        objednavkyView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                val item = (TextView) view.findViewById(R.id.objednavka_lokalna_apiId_textView);
+                Intent intent = new Intent(MojeObjednavkyActivity.this, ObjednavkaActivity.class);
+                intent.putExtra("idObjednavkyApi", Long.valueOf(String.valueOf(item.getText())));
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/sk/upjs/sk/illhave/activity/ObjednavkaActivity.java
+++ app/src/main/java/sk/upjs/sk/illhave/activity/ObjednavkaActivity.java
@@ -1,15 +1,51 @@
 package sk.upjs.sk.illhave.activity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
+import java.util.concurrent.ExecutionException;
+
+import lombok.val;
 import sk.upjs.sk.illhave.R;
+import sk.upjs.sk.illhave.async.GetOrderByIdAsyncTask;
+import sk.upjs.sk.illhave.entity.Order;
 
 public class ObjednavkaActivity extends AppCompatActivity {
+
+    Long idObjednavkyApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_objednavka);
+        ActivityCompat.requestPermissions(ObjednavkaActivity.this,
+                new String[]{Manifest.permission.INTERNET},
+                1);
+
+        val extras = getIntent().getExtras();
+        Order order = null;
+        if (extras != null) {
+            idObjednavkyApi = extras.getLong("idObjednavkyApi");
+            Log.e("objednavka: ", idObjednavkyApi.toString());
+            order = getOrderById(idObjednavkyApi);
+            Log.e("objednavka: ", order.toString());
+        }
+
+        TextView objednavkaTextView = findViewById(R.id.objednavkaDetail_textView);
+        objednavkaTextView.setText(order.toString());
+    }
+
+    private Order getOrderById(Long idObjednavkyApi) {
+        Order order = null;
+        try {
+            order = new GetOrderByIdAsyncTask().execute(idObjednavkyApi).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        return order;
     }
 }
